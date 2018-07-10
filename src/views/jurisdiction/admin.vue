24bb7a8ca9501488dc7c07cc35e46a75d9d45ff1
Liuxy0551 <34759874+liuxy0551@users.noreply.github.com>
2018-07-10 10:57:46 +0800

    管理员数据管理

--- src/views/jurisdiction/admin.vue
+++ src/views/jurisdiction/admin.vue
@@ -1,20 +1,136 @@
 <template>
-<div>
-  管理员管理
-</div>
+  <div class="m-user">
+    <page-title :title="name" @freshClick="freshClick"></page-title>
+    <div class="m-content">
+      <div class="m-top">
+        <div class="m-top-search">
+          <div class="m-top-text">用户ID：</div>
+          <el-input class="m-top-input" v-model="inputID" size="mini"></el-input>
+          <div class="m-top-text">用户名：</div>
+          <el-input class="m-top-input" v-model="inputName" size="mini"></el-input>
+          <el-button class="m-top-search-button" size="mini" @click="topSearch">查询</el-button>
+        </div>
+        <div class="m-top-button">
+          <el-button class="m-top-button-button" size="mini" @click="addAdmin">添加用户</el-button>
+        </div>
+      </div>
 
+      <div class="m-middle">
+        <el-table :data="admin" stripe style="width: 100%">
+          <el-table-column align="center" prop="id" label="编号"></el-table-column>
+          <el-table-column align="center" prop="userName" label="用户名"></el-table-column>
+          <el-table-column align="center" prop="nickName" label="昵称" width="180"></el-table-column>
+          <el-table-column align="center" prop="email" label="E-mall" width="180"></el-table-column>
+          <el-table-column align="center" prop="group" label="用户组"></el-table-column>
+          <el-table-column align="center" prop="status" label="状态"></el-table-column>
+          <el-table-column align="center" label="操作">
+            <template slot-scope="scope">
+              <el-dropdown size="mini" split-button>查看
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>
+                    <el-button type="text" class="el-crud" @click="editAdmin(scope.row)">编辑管理员信息</el-button>
+                  </el-dropdown-item>
+                  <el-dropdown-item>
+                    <el-button type="text" class="el-crud">封禁管理员</el-button>
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-dialog :title="editAdminTitle" :visible.sync="editAdminVisible" width="30%" show-close center>
+          <span>这是一段信息</span>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="editAdminVisible = false">取 消</el-button>
+            <el-button type="primary" @click="editAdminVisible = false">确 定</el-button>
+          </span>
+        </el-dialog>
+      </div>
+      <div class="m-bottom">
+        <pagination></pagination>
+      </div>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-    export default {
-        data() {
-            return {}
-        },
-        methods: {},
-        created() {
-
-        }
+  import pageTitle from '../../components/common/title';
+  import admin from '../../common/json/adminInfo'
+  import Pagination from "../../components/common/pages";
+  export default {
+    data() {
+      return {
+        name: '管理员数据管理',
+        inputID: '',
+        inputName: '',
+        admin: admin,
+        editAdminVisible: false,
+        editAdminTitle: ''
+      }
+    },
+    components:{
+      Pagination,
+      pageTitle
+    },
+    methods: {
+      freshClick(){
+        console.log('fresh');
+      },
+      topSearch() {
+        console.log('用户ID：', this.inputID)
+        console.log('用户名：', this.inputName)
+      },
+      addAdmin() {
+        console.log('添加用户')
+      },
+      editAdmin(row) {
+        this.editAdminVisible = true
+        this.editAdminTitle = row.id + ' 管理员数据管理'
+      }
+    },
+    created() {
+      // console.log(admin)
     }
+  }
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
-
+  @import "../../common/css/_variate.less";
+  .m-content {
+    padding: 0.2rem;
+    background: @bgMainColor;
+    .m-top {
+      .m-top-search {
+        margin: 0 0 0.1rem 0;
+        float: left;
+        width: 85%;
+        .m-top-text {
+          float: left;
+          line-height: 0.27rem;
+        }
+        .m-top-input {
+          float: left;
+          width: 12%;
+          margin-right: 0.15rem;
+        }
+        .m-top-search-button {
+          background: @btnActiveColor;
+          color: @bgMainColor;
+          margin-left: 0.1rem;
+        }
+      }
+      .m-top-button-button {
+        background: @btnActiveColor;
+        color: @bgMainColor;
+        float: right;
+      }
+    }
+    .m-middle {
+      .el-crud {
+        font-size: 0.14rem;
+        color: #000000;
+      }
+    }
+    .m-bottom {
+      margin: 0.2rem 0.4rem 0.3rem 0;
+    }
+  }
 </style>
