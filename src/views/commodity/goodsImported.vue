3e55ccca60e82308bb6762b358f72a0ef109ab7c
maoyaY <1335679005@qq.com>
2018-07-11 15:43:42 +0800

    商品发布页面样式

--- src/views/commodity/goodsImported.vue
+++ src/views/commodity/goodsImported.vue
@@ -6,33 +6,34 @@
       <div class="m-edit-btn">
         <span>-编辑-</span>
       </div>
+      <a name="basicInfo"></a>
       <div class="m-one-part">
         <h3>基本信息</h3>
-        <el-form-item label="活动名称:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+        <el-form-item label="商品名称:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
           <el-input v-model="form.name" class="m-input-l" ></el-input>
         </el-form-item>
-        <el-form-item label="活动名称:" :rules="[{ required: true, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
+        <el-form-item label="添加描述:" :rules="[{ required: true, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
           <el-input v-model="form.name" class="m-input-l" ></el-input>
           <p class="m-alert">建议描述文字在36字以内</p>
         </el-form-item>
-        <el-form-item label="活动名称:" :rules="[{ required: true}]">
+        <el-form-item label="商品属性:" :rules="[{ required: true}]">
           <div class="m-select-box">
             <p class="m-alert">错误填写商品属性，可能会引起商品下架或搜索流量减少，影响您的正常销售，请认真准确填写！</p>
             <div class="m-selects">
               <el-form-item label="活动名称名:" class="m-form-item" :rules="[{ required: false}]">
-                <el-input v-model="form.select" class="m-input-m" ></el-input>
+                <el-input v-model="form.select" class="m-input-s" ></el-input>
               </el-form-item>
               <el-form-item label="姓名：" class="m-form-item" :rules="[{ required: false}]">
-                <el-select v-model="form.select" class="m-input-m" >
+                <el-select v-model="form.select" class="m-input-s" >
                   <el-option label="区域一" value="shanghai"></el-option>
                   <el-option label="区域二" value="beijing"></el-option>
                 </el-select>
               </el-form-item>
               <el-form-item label="活动名称名:" class="m-form-item" :rules="[{ required: false}]">
-                <el-input v-model="form.select" class="m-input-m" ></el-input>
+                <el-input v-model="form.select" class="m-input-s" ></el-input>
               </el-form-item>
               <el-form-item label="姓名：" class="m-form-item" :rules="[{ required: false}]">
-                <el-select v-model="form.select" class="m-input-m" >
+                <el-select v-model="form.select" class="m-input-s" >
                   <el-option label="区域一" value="shanghai"></el-option>
                   <el-option label="区域二" value="beijing"></el-option>
                 </el-select>
@@ -40,7 +41,7 @@
             </div>
           </div>
         </el-form-item>
-        <el-form-item label="活动名称:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+        <el-form-item label="商品照片:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
           <el-upload
             action="https://jsonplaceholder.typicode.com/posts/"
             list-type="picture-card"
@@ -52,12 +53,179 @@
           <el-dialog :visible.sync="dialogVisible">
             <img width="100%" :src="dialogImageUrl" alt="">
           </el-dialog>
+          <p class="m-img-p">建议尺寸：700*700像素，可以通过拖动图片调整前后顺序，最多上传5张商品图片</p>
+        </el-form-item>
+
+        <el-form-item label="商品样式及价格:" :rules="[{ required: true}]">
+          <div class="m-product-style">
+            <div class="m-product">
+              <div class="m-product-input">
+                <el-input v-model="form.name" placeholder="输入商品颜色，如粉红色" class="m-input-m" ></el-input>
+                <el-input v-model="form.name" placeholder="输入商品规格，如进阶版" class="m-input-m" ></el-input>
+                <el-input v-model="form.name" placeholder="输入商品该规格价格" class="m-input-m" ></el-input>
+              </div>
+              <div>
+                <el-upload
+                  action="https://jsonplaceholder.typicode.com/posts/"
+                  list-type="picture-card"
+                  :on-preview="handlePictureCardPreview"
+                  :on-remove="handleRemove"
+                  class="m-img-l">
+                  <span>+添加图片</span>
+                </el-upload>
+              </div>
+            </div>
+            <div class="m-product">
+              <div class="m-product-input">
+                <el-input v-model="form.name" placeholder="输入商品颜色，如粉红色" class="m-input-m" ></el-input>
+                <el-input v-model="form.name" placeholder="输入商品规格，如进阶版" class="m-input-m" ></el-input>
+                <el-input v-model="form.name" placeholder="输入商品该规格价格" class="m-input-m" ></el-input>
+              </div>
+              <div>
+                <el-upload
+                  action="https://jsonplaceholder.typicode.com/posts/"
+                  list-type="picture-card"
+                  :on-preview="handlePictureCardPreview"
+                  :on-remove="handleRemove"
+                  class="m-img-l">
+                  <span>+添加图片</span>
+                </el-upload>
+              </div>
+            </div>
+            <p class="m-add-more">+查看更多</p>
+            <p class="m-look-more">更多基本信息设置</p>
+          </div>
+        </el-form-item>
+        <el-form-item label="商品卖点:" :rules="[{ required: false, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
+          <el-input v-model="form.name" class="m-input-l" placeholder="输入内容"></el-input>
+          <p class="m-alert">在商品详情页下面展示卖点信息，建议36字以内，<span class="m-link">查看示例</span></p>
+        </el-form-item>
+        <el-form-item label="商品视频:" :rules="[{ required: false, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+            class="m-img-l">
+            <span>+添加视频</span>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+          <p class="m-img-p">建议视频尺寸比例为1:1或16:9，时长不超过60秒</p>
+        </el-form-item>
+      </div>
+      <a name="priceInfo"></a>
+      <div class="m-one-part">
+        <h3>商品库存</h3>
+        <el-form-item label="划线价格:" :rules="[{ required: false, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
+          <el-input v-model="form.name" class="m-input-m" ></el-input>
+          <p class="m-alert">建议描述文字在36字以内</p>
+        </el-form-item>
+        <el-form-item label="库存:" :rules="[{ required: false, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
+          <el-input v-model="form.name" class="m-input-m" ></el-input>
+          <div class="m-check-content">
+            <el-checkbox v-model="checked"></el-checkbox>
+            <span>商品详情不显示剩余件数</span>
+          </div>
+          <p class="m-alert">库存为0时，会放入已售罄列表中</p>
+          <p class="m-look-more">更多价格库存设置</p>
+        </el-form-item>
+        <el-form-item label="成本价格:" :rules="[{ required: false, message: '年龄不能为空'}, { type: 'number', message: '年龄必须为数字值'}]">
+          <el-input v-model="form.name" class="m-input-m" ></el-input>
+          <p class="m-alert">建议描述文字在36字以内</p>
+        </el-form-item>
+
+      </div>
+
+      <a name="otherInfo"></a>
+      <div class="m-one-part m-other">
+        <h3>其他信息</h3>
+        <el-form-item label="快递运费:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+          <el-radio v-model="radio" label="1">统一邮费  </el-radio>
+          <el-input v-model="form.name" class="m-input-s" ></el-input>
+        </el-form-item>
+        <el-form-item label="上架时间:" :rules="[{ required: true, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+          <div>
+            <el-radio v-model="radio" label="1">立即上架售卖  </el-radio>
+          </div>
+          <div>
+            <el-radio v-model="radio" label="0">自定义上架时间  </el-radio>
+            <el-input v-model="form.name" class="m-input-s" ></el-input>
+            <p class="m-alert">需等待管理员审批完成，上架时间请选择24小时以后</p>
+          </div>
+          <p class="m-look-more">更多其他信息设置</p>
+        </el-form-item>
+        <el-form-item label="限购:" :rules="[{ required: false, message: '年龄不能为空'},{ type: 'number', message: '年龄必须为数字值'}]">
+          <div>
+            <el-radio v-model="radio" label="1">无现货，下单后需过段时间才能发货  </el-radio>
+          </div>
+          <div>
+            <el-radio v-model="radio" label="0">只允许特定用户购买  </el-radio>
+          </div>
+          <div>
+            <el-radio v-model="radio" label="0">允许用户购买数量  </el-radio>
+            <el-input v-model="form.name" class="m-input-s" placeholder="每个ID限购5个"></el-input>
+          </div>
         </el-form-item>
       </div>
 
+      <div class="m-goodsImported-foot">
+        <span class="m-foot-btn">保存并查看</span>
+        <span class="m-foot-btn">发布</span>
+      </div>
     </div>
-
-
+    <div class="m-right-side">
+      <div class="m-step-box">
+        <ul>
+          <li>
+            <span class="circle m-both-ends"></span>
+            <span class="m-step-content"></span>
+          </li>
+          <li>
+            <span class="line">|</span>
+            <span class="m-step-content"></span>
+          </li>
+          <li @click="sideClick('basicInfo')">
+            <a href="#basicInfo">
+              <span class="circle" :class="linkTo == 'basicInfo'?'active':''"></span>
+              <span class="m-step-content">1、基本信息</span>
+            </a>
+          </li>
+          <li>
+            <span class="line">|</span>
+            <span class="m-step-content"></span>
+          </li>
+          <li @click="sideClick('priceInfo')">
+            <a href="#priceInfo">
+              <span class="circle" :class="linkTo == 'priceInfo'?'active':''"></span>
+              <span class="m-step-content">2、商品库存</span>
+            </a>
+          </li>
+          <li>
+            <span class="line">|</span>
+            <span class="m-step-content"></span>
+          </li>
+          <li @click="sideClick('otherInfo')">
+            <a href="#otherInfo">
+              <span class="circle " :class="linkTo == 'otherInfo'?'active':''"></span>
+              <span class="m-step-content">3、其他信息</span>
+            </a>
+          </li>
+          <li>
+            <span class="line">|</span>
+            <span class="m-step-content"></span>
+          </li>
+          <li>
+            <span class="circle m-both-ends"></span>
+            <span class="m-step-content"></span>
+          </li>
+        </ul>
+      </div>
+      <div class="m-side-btn">
+        <span class="m-btn">保存</span>
+      </div>
+    </div>
   </el-form>
 </div>
 
@@ -73,7 +241,10 @@
                 select:''
               },
               dialogImageUrl: '',
-              dialogVisible: false
+              dialogVisible: false,
+              checked:true,
+              radio:0,
+              linkTo:'basicInfo'
             }
         },
       components:{
@@ -89,6 +260,9 @@
           handlePictureCardPreview(file) {
             this.dialogImageUrl = file.url;
             this.dialogVisible = true;
+          },
+          sideClick(v){
+            this.linkTo = v;
           }
         },
         created() {
@@ -98,11 +272,34 @@
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
   @import "../../common/css/_variate";
-.m-input-l{
-  width: 4.8rem;
-}
+  @import "../../common/css/index";
+  .m-input-l{
+    width: 4.8rem;
+  }
   .m-input-m{
-    width: 1.9rem;
+    width: 2.8rem;
+  }
+  .m-input-s{
+    width: 1.8rem;
+  }
+  .m-other{
+    .m-input-s{
+      margin-left: 0.1rem;
+    }
+  }
+  .m-img-p{
+    color: @greyColor;
+    font-size: 0.12rem;
+  }
+  .m-link{
+    color: @green;
+    cursor: pointer;
+  }
+  .m-look-more{
+    font-size: 0.12rem;
+    color: @green;
+    cursor: pointer;
+    margin-top: 0.1rem;
   }
   .m-goodsImported-content{
     background-color: #fff;
@@ -121,7 +318,12 @@
         font-weight: 600;
         margin-bottom: 0.2rem;
       }
-      p.m-alert{
+      .m-alert{
+        font-size: 0.12rem;
+        color: @greyColor;
+        line-height: 0.18rem;
+      }
+      .m-check-content{
         font-size: 0.12rem;
         color: @greyColor;
         line-height: 0.18rem;
@@ -147,9 +349,104 @@
           .m-img-l{
             font-size: 0.14rem;
             color: @green;
+
+          }
+        }
+      }
+      .m-product-style{
+        .m-product{
+          display: flex;
+          flex-flow: row;
+          align-items: center;
+          .m-product-input{
+            display: flex;
+            flex-flow: column;
+            margin-right: 0.2rem;
+            margin-bottom: 0.1rem;
+          }
+        }
+        .m-add-more{
+          font-size: 0.12rem;
+          color: @green;
+          padding-left: 0.8rem;
+          cursor: pointer;
+        }
+      }
+    }
+    .m-goodsImported-foot{
+      margin: 1rem 0.5rem 0;
+      border-top: 1px solid @borderColor;
+      height: 2rem;
+      .flex-row(space-around);
+      .m-foot-btn{
+        display: block;
+        width: 2rem;
+        height: 0.5rem;
+        line-height: 0.5rem;
+        color: #fff;
+        text-align: center;
+        font-size: 0.18rem;
+        border-radius: 5px;
+        background-color: @btnActiveColor;
+      }
+    }
+   }
+  .m-right-side{
+    position: fixed;
+    right: 0.4rem;
+    top: 50%;
+    font-size: 0.12rem;
+    .m-step-box{
+      width: 1rem;
+      ul{
+        li{
+          .flex-row(flex-start);
+          a{
+            .flex-row(flex-start);
+          }
+          span{
+            display: block;
+            color: @greyColor;
+            cursor: pointer;
+            &.circle{
+              width: 0.15rem;
+              height: 0.15rem;
+              border-radius: 50%;
+              border: 1px solid @borderColor;
+              background-color: @borderColor;
+              margin-right: 0.1rem;
+
+              &.m-both-ends{
+                background-color: #fff;
+                cursor: inherit;
+              }
+              &.active{
+                background-color: @btnActiveColor;
+              }
+            }
+            &.line{
+              width: 0.15rem;
+              text-align: center;
+              color: @borderColor;
+              line-height: 0.15rem;
+            }
+
           }
         }
       }
     }
-   }
+    .m-side-btn{
+      text-align: center;
+      span.m-btn{
+        display: inline-block;
+        width: 0.8rem;
+        height: 0.3rem;
+        line-height: 0.3rem;
+        background-color: @btnActiveColor;
+        color: #fff;
+        border-radius: 5px ;
+        margin-top: 0.2rem;
+      }
+    }
+  }
 </style>
