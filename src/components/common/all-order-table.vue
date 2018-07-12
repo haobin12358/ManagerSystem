57c0e6d4028f27eb66151c86e8182666004cad47
Liuxy0551 <34759874+liuxy0551@users.noreply.github.com>
2018-07-12 13:37:51 +0800

    所有订单

--- src/components/common/all-order-table.vue
+++ src/components/common/all-order-table.vue
@@ -1,11 +1,15 @@
 <template>
+  <div class="order-table">
+    <div class="order-table-header">
 
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     props: [],
-    name: "all-order",
+    name: "all-order-table",
     data() {
       return {
 
@@ -19,5 +23,8 @@
 
 <style lang="less" rel="stylesheet/less" scoped>
   @import "../../common/css/_variate.less";
+  .order-table {
+
+  }
 
 </style>
