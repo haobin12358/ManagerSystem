b08cfa7ba1189673df663fb99183b0a7d9c39981
Liuxy <liuxy0551@qq.com>
2018-07-19 16:48:34 +0800

    所有订单

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const api={
   login: title + 'user/login',//用户登录
   changePwd: title + 'user/update_user',//修改密码
 
-  get_all_order: title + 'order/order_list',//获取所有商品
+  get_all_order: title + 'order/order_list',//获取所有订单
 
   get_all_product: title + 'product/get_all',//获取所有商品
   update_product: title + 'product/update_product',//上架/下架商品
