ca6ae83f3060ddc9f444375d621bcee871edca6a
maoyaY <1335679005@qq.com>
2018-07-24 10:40:49 +0800

    baocun‘

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-const title = 'http://10.0.0.197:7443/sharp/manager/';
-// const title = 'http://120.79.182.43:7443/sharp/manager/';
+// const title = 'http://10.0.0.197:7443/sharp/manager/';
+const title = 'http://120.79.182.43:7443/sharp/manager/';
 
 const api={
   login: title + 'user/login',//用户登录
@@ -13,6 +13,7 @@ const api={
   get_all_product: title + 'product/get_all',//获取所有商品
   release_product: title + 'product/release',//发布商品商品
   update_product : title + 'product/update_product',//上下架商品
+  upload_files: title +'other/upload_files',//上传图片
 
   get_first_category: title + 'category/get_first',//获取类目
   get_child_category:title + 'category/get_child',//获取子类目
